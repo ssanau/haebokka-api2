2bbdf5fa89305346a58f3d58031ec2a46b152241
HYUNASEO <hyunaseodev@gmail.com>
2022-11-20 17:24:56 +0900

    first commit

---
+++ src/main/java/com/ssanawoo/Haebokka/HaebokkaApplication.java
@@ -0,0 +1,13 @@
+package com.ssanawoo.Haebokka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaebokkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaebokkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssanawoo/Haebokka/HaebokkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssanawoo.Haebokka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaebokkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
